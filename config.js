abd0d3521ee13f3dafffd2c228aab36e9ddfddd3
Subrodeep <125560440+Subrodeep9732@users.noreply.github.com>
2025-02-10 14:50:15 +0530

    config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
   embedColor: "#1db954",
   activityName: "Royal S Gaming Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.com/invite/gth7Byz46A",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
