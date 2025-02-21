4374a868bd01662d465622491a21f61524ca1db4
Subrodeep <125560440+Subrodeep9732@users.noreply.github.com>
2025-02-22 01:44:08 +0530

    config.js

--- config.js
+++ config.js
@@ -3,22 +3,22 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  ownerID: ["733513636269785120", ""], 
+  mongodbUri : "mongodb+srv://subrodeep:Subro9732@discordbot.a5lxc.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "Royal S Gaming Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.com/invite/royalsgaming",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
      {
-      name: "XX",
-      password: "enteryourcustompass",
-      host: "180.188.226.76",
-      port:  7019,
+      name: "INZEWORLD.COM (DE)",
+      password: "saher.inzeworld.com",
+      host: "lava.inzeworld.com",
+      port:  3128,
       secure: false
     }
   ]
